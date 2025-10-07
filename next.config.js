ffbd0aa7207824288548ba876c7f917b87a50daa
denc239 <46786447+denc239@users.noreply.github.com>
2025-10-07 14:00:27 +0300

    Добавлен next.config.js проекта CS_Freedome v2 (повторная попытка)

---
+++ next.config.js
@@ -0,0 +1,11 @@
+export const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['cdn.example.com'],
+  }
+};
+
+export default nextConfig;
